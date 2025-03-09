e3b17696975cea0762c68d0e849a4ff4c1e6d77a
Félix Mariotto <felix.mariotto@gmail.com>
2025-03-09 16:38:31 +0100

    create uid files for each script, and disociate players movement with two sets of input

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
+@export var PLAYER_ID = 0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,32 +12,47 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("player0_jump") and PLAYER_ID == 0 and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	elif Input.is_action_just_pressed("player1_jump") and PLAYER_ID == 1 and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 	
-	# Animation handling
-	if not is_on_floor():
-		var realVelocity = get_real_velocity()
-		if realVelocity.y > 0 :
-			$AnimatedSprite2D.play("fall")
-		else :
-			$AnimatedSprite2D.play("jump")
-	else:
-		if direction:
-			$AnimatedSprite2D.play("run")
-			$AnimatedSprite2D.flip_h = velocity.x < 0
+	
+	for i in 2:
+		
+		if PLAYER_ID != i:
+			continue
+		
+		# Get the input direction and handle the movement/deceleration.
+		
+		var direction = Input.get_axis( "player%s_left" % i, "player%s_right" % i )
+		
+		if direction > 0.0:
+			velocity.x = 1.0 * SPEED
+		elif direction < 0.0:
+			velocity.x = -1.0 * SPEED
 		else:
-			$AnimatedSprite2D.play("idle")
+			velocity.x = 0.0
+	
+		move_and_slide()
+	
+		# Animation handling
+		if not is_on_floor():
+			
+			var realVelocity = get_real_velocity()
+			
+			if realVelocity.y > 0 :
+				$AnimatedSprite2D.play("fall")
+			else :
+				$AnimatedSprite2D.play("jump")
+				
+		else:
+			
+			if direction:
+				$AnimatedSprite2D.play("run")
+				$AnimatedSprite2D.flip_h = velocity.x < 0
+			else:
+				$AnimatedSprite2D.play("idle")
 
 
 func _on_collectibles_collected(message: String) -> void:
