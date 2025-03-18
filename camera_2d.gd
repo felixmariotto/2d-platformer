d7eaf078c88b304f5d6d337799c3e28c488ace3b
Félix Mariotto <felix.mariotto@gmail.com>
2025-03-18 23:17:50 +0100

    fix unsmooth transition from merged to splitted screen

--- camera_2d.gd
+++ camera_2d.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var viewportRect = get_viewport_rect().size
-	var playersAxis = ( target0.position - target1.position ).normalized()
+	var playersAxis = ( ( target0.position - target1.position ) / viewportRect ).normalized()
 	
 	var targetPos;
 	
